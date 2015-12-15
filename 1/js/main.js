2b9ec030ef239509bae59816bfc107d21f5b8e84
Roxana <ungureanu.roxi@gmail.com>
2015-12-15 14:05:48 +0200

    assignment1

---
+++ 1/js/main.js
@@ -0,0 +1,15 @@
+bar = document.getElementById("bar");
+btn = document.getElementById("btn");
+
+var getRandomNumber = function () {
+	return Math.floor (Math.random()*100);
+};
+
+btn.addEventListener("click", function () {
+	var result = getRandomNumber() + "%";
+	bar.style.width = result;
+}
+);
+
+
+
